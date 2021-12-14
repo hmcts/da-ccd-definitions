624acdd5e8be15a3c8f5763ab8e753300247be75
Jahnavi <jahnavi1satya@gmail.com>
2021-12-14 08:53:41 +0000

    Add Sample Config file

--- test/end-to-end/config.js
+++ test/end-to-end/config.js
@@ -1,6 +1,39 @@
+const defaultPassword = 'Password123';
+
 module.exports = {
-  legalProfessionalUserOne: {
-    email: 'da_caseworker_solicitor@mailinator.com'
+  solicitorDAUser: {
+    email: 'da_caseworker_solicitor@mailinator.com',
+    password: process.env.DA_USER_PASSWORD || defaultPassword
+  },
+  courtAdmDAUser: {
+    email: 'da_caseworker_courtadm@mailinator.com',
+    password: process.env.DA_USER_PASSWORD || defaultPassword
+  },
+  judgeDAUser: {
+    email: 'da_caseworker_judge@mailinator.com',
+    password: process.env.DA_USER_PASSWORD || defaultPassword
+  },
+  localAuthourityDAUser: {
+    email: 'da_caseworker_la@mailinator.com',
+    password: process.env.LA_USER_PASSWORD || defaultPassword
+  },
+  superUserDA: {
+    email: 'da_caseworker_superuser@mailinator.com',
+    password: process.env.LA_USER_PASSWORD || defaultPassword
+  },
+  orgManagerDAUser: {
+    email: 'da_org_manager@mailinator.com',
+    password: process.env.LA_USER_PASSWORD || defaultPassword
+  },
+  
+  idamApiUrl: process.env.IDAM_API_URL || 'http://localhost:5000',
+  TestOutputDir: process.env.E2E_OUTPUT_DIR || './output',
+
+  definition: {
+    jurisdiction: 'PRIVATELAW',
+    jurisdictionFullDesc: 'Family Private Law',
+    caseType: 'TBC',
+    caseTypeFullDesc: 'C100 Application',
   },
 
   baseUrl: process.env.URL || 'https://manage-case.aat.platform.hmcts.net/cases'
